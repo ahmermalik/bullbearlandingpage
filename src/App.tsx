747b62b473acf7b1ef3255e3e40438ab609df14a
Ahmer Malik <ahmermalik@Ahmers-Mac-mini.local>
2023-07-26 21:39:40 -0500

    fixed scrolling to sections

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,19 @@ function App() {
       <NavBar section={section} setSection={setSection} />
       <section className="home currentsection">
         {" "}
-        current section is {section}
+        current section is home
       </section>
       <section className="about">
         {" "}
-        current section is {section}
+        current section is about
       </section>
       <section className="why">
         {" "}
-        current section is {section}
+        current section is why
       </section>
       <section className="roadmap">
         {" "}
-        current section is {section}
+        current section is roadmap
       </section>
     </div>
   );
