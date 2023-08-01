60bba610232da8552bd9ab90d207d06b92f3e63d
Ahmer Malik <ahmermalik@Ahmers-Mac-mini.local>
2023-07-31 23:04:35 -0500

    added grids and template text img

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
 import NavBar from "./components/Navbar";
+import Grid from '@mui/material/Grid';
+import supper from "./images/lastsupper/finalcut.png"
+
 
 function App() {
   const [section, setSection] = useState("Home");
@@ -9,11 +12,23 @@ function App() {
   return (
     <div className="app">
       
-      <NavBar section={section} setSection={setSection} />
+      <NavBar section={section} setSection={setSection}/>
       <div className="main-content">
       <section className="home">
-        {" "}
-        current section is home
+     
+        {" current section is home "}
+        <Grid container spacing={2}>
+  <Grid item md={5}>
+   <div  className="supper"> <img className="supper-img" src={supper} alt="Logo" /> </div>
+  </Grid>
+  <Grid item md={7}>
+    <h3> Automate your TA</h3>
+  <h1> Stop spending hours on manually checking your watch lists</h1>
+  <h3> All-in-one TA tool to save you time</h3>
+  
+  </Grid>
+</Grid>
+
       </section>
       <section className="about">
         {" "}
@@ -21,7 +36,7 @@ function App() {
       </section>
       <section className="why">
         {" "}
-        current section is why
+        current section is why BBB will help you
       </section>
       <section className="roadmap">
         {" "}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -56,7 +56,7 @@ export default function Navbar(props: Props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex' }} style={{ marginBottom: '60px' }}>
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
